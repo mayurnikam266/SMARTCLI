71bd08cdc4bce9eb9cd2566bea5765b8ad78cabe
Mayur Nikam <mayurnikam266@gmail.com>
2025-07-17 00:12:42 +0530

    changed groq llm file

--- llm_engines/groq_llm.py
+++ llm_engines/groq_llm.py
@@ -1,17 +1,20 @@
-import openai
+from openai import OpenAI
 
 def get_response(prompt, config):
-    openai.api_key = config.get("api_key")
-    model = config.get("model", "llama3-8b")
-    temperature = config.get("temperature", 0.7)
-
     try:
-        response = openai.ChatCompletion.create(
-            base_url="https://api.groq.com/openai/v1",
+        client = OpenAI(
+            api_key=config.get("api_key"),
+            base_url="https://api.groq.com/openai/v1"
+        )
+        model = config.get("model", "llama3-8b")
+        temperature = config.get("temperature", 0.7)
+
+        response = client.chat.completions.create(
             model=model,
             messages=[{"role": "user", "content": prompt}],
             temperature=temperature
         )
         return response.choices[0].message.content.strip()
+
     except Exception as e:
         return f"Groq error: {e}"
